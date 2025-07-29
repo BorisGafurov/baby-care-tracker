2c69cbb14cb2b85f1909c4be7a9f6ad5a42c02f5
BORIS GAFUROV <gafur08@yandex.ru>
2025-07-29 23:32:12 +0400

    Поправки16

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === 'production' ? '/baby-care-tracker/' : '/',
+export default defineConfig({
+    base: '/baby-care-tracker/',
     plugins: [react()],
     build: {
-      outDir: 'dist'
+      outDir: 'dist',
+      emptyOutDir: true,
+      sourcemap: true 
     }
-  };
 });
